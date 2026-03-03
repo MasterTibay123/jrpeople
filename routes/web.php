2fb4dbdba98c529a6bd4b8827ad9ca6d35c97e91
Jay-Ar Tibay <87292001+MasterTibay123@users.noreply.github.com>
2026-03-03 10:12:09 +0800

    Uncomment routes for PersonController

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ImageController;
 
-//Route::get('/', [PersonController::class, 'view'])->name('people.view');
-//Route::post('/people/store', [PersonController::class, 'store'])->name('people.store');
-//Route::put('/people/{id}', [PersonController::class, 'update'])->name('people.update');
-//Route::delete('/people/{id}', [PersonController::class, 'destroy'])->name('people.destroy');
+Route::get('/', [PersonController::class, 'view'])->name('people.view');
+Route::post('/people/store', [PersonController::class, 'store'])->name('people.store');
+Route::put('/people/{id}', [PersonController::class, 'update'])->name('people.update');
+Route::delete('/people/{id}', [PersonController::class, 'destroy'])->name('people.destroy');
 
 Route::get('/images', [ImageController::class, 'index']);
 Route::post('/upload-image', [ImageController::class, 'upload']);
